0f53a7ad9e44b019de240d3c6cda18f1e2cdca3a
creatjava <2575134399@qq.com>
2023-05-04 20:02:49 +0800

    544545

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -1,84 +1,66 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdbool.h>
 
-#define MAX_FILENAME_LEN 256
-#define MAX_BUFFER_LEN 1024
-
-// 统计字符数或单词数
-int wordCount(char* filename, char* param) {
-    FILE* fp;
-    int count = 0;
-    char buffer[MAX_BUFFER_LEN];
-    int in_word = 0;
-
-    fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Error opening file.\n");
-        return -1;
+void count_chars(FILE* file, int* char_count) {
+    char ch;
+    *char_count = 0;
+    while ((ch = fgetc(file)) != EOF) {
+        (*char_count)++;
     }
+}
 
-    while (fgets(buffer, MAX_BUFFER_LEN, fp) != NULL) {
-        int len = strlen(buffer);
-        for (int i = 0; i < len; i++) {
-            if (param[1] == 'c') {  // 统计字符数
-                count++;
-            }
-            else if (isspace(buffer[i]) || buffer[i] == ',' || buffer[i] == '.' || buffer[i] == '\n') {
-                if (in_word) {  // 当前字符是分隔符并且上一个字符是单词字符
-                    count++;
-                    in_word = 0;
-                }
-            }
-            else {
-                in_word = 1;
+void count_words(FILE* file, int* word_count) {
+    char ch;
+    bool in_word = false;
+    *word_count = 0;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == ' ' || ch == ',' || ch == '\t' || ch == '\n') {
+            if (in_word) {
+                (*word_count)++;
+                in_word = false;
             }
         }
+        else {
+            in_word = true;
+        }
     }
-
-    fclose(fp);
-    return count;
+    if (in_word) {
+        (*word_count)++;
+    }
 }
 
 int main(int argc, char* argv[]) {
-    char filename[MAX_FILENAME_LEN] = "input.txt";
-    char param[3];
-    int count;
+    if (argc != 3) {
+        printf("Usage: %s [-c|-w] input_file_name\n", argv[0]);
+        return 1;
+    }
 
-    if (argc == 1) {
-        printf("Usage: WordCount [-c | -w] [input_file_name]\n");
-        return 0;
+    char* parameter = argv[1];
+    char* input_file_name = argv[2];
+
+    FILE* file = fopen(input_file_name, "r");
+    if (file == NULL) {
+        printf("Error opening file: %s\n", input_file_name);
+        return 1;
     }
-    else if (argc == 2) {
-        strcpy(param, argv[1]);
+
+    int result;
+    if (strcmp(parameter, "-c") == 0) {
+        count_chars(file, &result);
+        printf("字符数: %d\n", result);
     }
-    else if (argc == 3) {
-        strcpy(param, argv[1]);
-        strcpy(filename, argv[2]);
+    else if (strcmp(parameter, "-w") == 0) {
+        count_words(file, &result);
+        printf("单词数: %d\n", result);
     }
     else {
-        printf("Invalid arguments.\n");
-        printf("Usage: WordCount [-c | -w] [input_file_name]\n");
-        return 0;
+        printf("Invalid parameter: %s\n", parameter);
+        printf("Usage: %s [-c|-w] input_file_name\n", argv[0]);
+        return 1;
     }
 
-    if (strcmp(param, "-c") != 0 && strcmp(param, "-w") != 0) {
-        printf("Invalid parameter.\n");
-        printf("Usage: WordCount [-c | -w] [input_file_name]\n");
-        return 0;
-    }
-
-    count = wordCount(filename, param);
-    if (count == -1) {
-        return 0;
-    }
-
-    if (param[1] == 'c') {
-        printf("Characters: %d\n", count);
-    }
-    else {
-        printf("Words: %d\n", count);
-    }
-
+    fclose(file);
     return 0;
 }
